755e1300f4751e8c674dd9e0b20217fdebba7cf9
YU Di <38583300+yxkwgp@users.noreply.github.com>
2026-01-20 13:18:58 +0800

    Update prep_MHs.py

--- core/prep_MHs.py
+++ core/prep_MHs.py
@@ -68,14 +68,14 @@ class MicrohaplotypeGenerator:
         if r2_threshold < 0 or r2_threshold > 1:
             raise ValueError("r2_threshold must be between 0 and 1")
         
-        print(f"Starting microhaplotype generation, length threshold: {length_threshold}, minimum sites: {min_site_number}")
+        print(f'Starting microhaplotype generation, length threshold: {length_threshold}, minimum sites: {min_site_number}')
         
         # plink calculate linkage disequilibrium
         length_threshold_kb = length_threshold / 1000
-        cmd = f'plink --bfile {input_prefix} --extract {snplist} --r2 --ld-window 10000 --ld-window-kb {length_threshold_kb} --ld-window-r2 0 --out {input_prefix}'
+        ld_file = os.path.basename(input_prefix) + '.ld'
+        cmd = f'plink --bfile {input_prefix} --extract {snplist} --r2 --ld-window 10000 --ld-window-kb {length_threshold_kb} --ld-window-r2 0 --out {os.path.basename(input_prefix)}'
         run_plink_command(cmd)
 
-        ld_file = f'{input_prefix}.ld'
         if not os.path.exists(ld_file):
             raise RuntimeError(f"Linkage disequilibrium file not generated: {ld_file}")
         
@@ -87,9 +87,11 @@ class MicrohaplotypeGenerator:
         infile2.readline()
         
         # First line of ld file
+        screen_rem_chr = []
         anchor_line = infile1.readline().strip().split()
+        screen_rem_chr.append(anchor_line[0])
         site_group = []
-        if float(anchor_line[6]) < r2_threshold and int(anchor_line[1]) + length_threshold > int(anchor_line[4]):
+        if float(anchor_line[6]) <= r2_threshold and int(anchor_line[1]) + length_threshold > int(anchor_line[4]):
             site_group.append(anchor_line)
         
         # Initialize ld records
@@ -105,9 +107,14 @@ class MicrohaplotypeGenerator:
         for line in infile1:
             line = line.strip().split()
             if line[2] == anchor_line[2]:
-                if float(line[6]) < r2_threshold and int(line[1]) + length_threshold > int(line[4]):
+                if float(line[6]) <= r2_threshold and int(line[1]) + length_threshold > int(line[4]):
                     site_group.append(line)
             else:
+                if line[0] not in screen_rem_chr:
+                    screen_rem_chr.append(line[0])
+                    ld_rem_pos = []
+                    ld_rem_pair = []
+                    ld_rem_r2 = []
                 if len(site_group) >= min_site_number - 1:
                     mh_pri = set([site_group[0][2]] + [site_group[i][5] for i in range(len(site_group))])
                     if self._is_not_subset_any(mh_pri, mhs_pri_rem):
@@ -118,7 +125,7 @@ class MicrohaplotypeGenerator:
                         front_site_info = [site_group[0][i] for i in [2, 0, 1]]  # [rsID, chr, bp]
                         headless_mh_waiting_check = [site_group[i][5] for i in range(len(site_group))]
                         
-                        while ld_line and len(ld_line) > 0 and ld_line[0] != front_site_info[1]:
+                        while ld_line and len(ld_line) > 0 and ld_line[0] != front_site_info[1] and ld_line[0] in screen_rem_chr:
                             next_line = infile2.readline()
                             if not next_line:  # End of file
                                 break
@@ -132,7 +139,6 @@ class MicrohaplotypeGenerator:
                             if not next_line:  # End of file
                                 break
                             ld_line = next_line.strip().split()
-                        
                         pair_index = 0
                         while pair_index < len(ld_rem_pos) and ld_rem_pos[pair_index] < int(front_site_info[2]):
                             pair_index += 1
@@ -140,16 +146,17 @@ class MicrohaplotypeGenerator:
                         ld_rem_pos = ld_rem_pos[pair_index:]
                         ld_rem_pair = ld_rem_pair[pair_index:]
                         ld_rem_r2 = ld_rem_r2[pair_index:]
-                        
                         unmatch = [set() for i in headless_mh_waiting_check]
                         for m in range(len(headless_mh_waiting_check) - 1):
                             for n in range(m + 1, len(headless_mh_waiting_check)):
                                 pair = f'{headless_mh_waiting_check[m]}-{headless_mh_waiting_check[n]}'
                                 if pair in ld_rem_pair:
                                     r2 = ld_rem_r2[ld_rem_pair.index(pair)]
-                                    if r2 < r2_threshold:
+                                    if r2 <= r2_threshold:
                                         unmatch[m].add(n)
                                         unmatch[n].add(m)
+                                else:
+                                    raise ValueError("pair not found")
                         
                         cliques = self._find_cliques(unmatch)
                         for clique in cliques:
@@ -159,15 +166,65 @@ class MicrohaplotypeGenerator:
                                 headless_mh_formal = [headless_mh_waiting_check[i] for i in clique]
                                 outfile.write(f'{front_site_info[1]}|{front_site_info[0]}-{"-".join(headless_mh_formal)}\n')
                 
-                # Update anchorLine
+                # Update anchor_line
                 site_group = []
                 anchor_line = line[:]
-                if float(anchor_line[6]) < r2_threshold:
+                if float(anchor_line[6]) <= r2_threshold:
                     site_group.append(anchor_line)
-        
+        else:
+            if len(site_group) >= min_site_number - 1:
+                mh_pri = set([site_group[0][2]] + [site_group[i][5] for i in range(len(site_group))])
+                if self._is_not_subset_any(mh_pri, mhs_pri_rem):
+                    mhs_pri_rem.append(mh_pri)
+                    if len(mhs_pri_rem) > length_threshold:
+                        mhs_pri_rem = mhs_pri_rem[1:]
+                        
+                    front_site_info = [site_group[0][i] for i in [2, 0, 1]]  # [rsID, chr, bp]
+                    headless_mh_waiting_check = [site_group[i][5] for i in range(len(site_group))]
+                        
+                    while ld_line and len(ld_line) > 0 and ld_line[0] != front_site_info[1] and ld_line[0] in screen_rem_chr:
+                        next_line = infile2.readline()
+                        if not next_line:  # End of file
+                            break
+                        ld_line = next_line.strip().split()
+                        
+                    while ld_line and len(ld_line) > 0 and ld_line[0] == front_site_info[1] and int(ld_line[1]) < int(front_site_info[2]) + length_threshold:
+                        ld_rem_pos.append(int(ld_line[1]))
+                        ld_rem_pair.append(f'{ld_line[2]}-{ld_line[5]}')
+                        ld_rem_r2.append(float(ld_line[6]))
+                        next_line = infile2.readline()
+                        if not next_line:  # End of file
+                            break
+                        ld_line = next_line.strip().split()
+                    pair_index = 0
+                    while pair_index < len(ld_rem_pos) and ld_rem_pos[pair_index] < int(front_site_info[2]):
+                        pair_index += 1
+                        
+                    ld_rem_pos = ld_rem_pos[pair_index:]
+                    ld_rem_pair = ld_rem_pair[pair_index:]
+                    ld_rem_r2 = ld_rem_r2[pair_index:]
+                    unmatch = [set() for i in headless_mh_waiting_check]
+                    for m in range(len(headless_mh_waiting_check) - 1):
+                        for n in range(m + 1, len(headless_mh_waiting_check)):
+                            pair = f'{headless_mh_waiting_check[m]}-{headless_mh_waiting_check[n]}'
+                            if pair in ld_rem_pair:
+                                r2 = ld_rem_r2[ld_rem_pair.index(pair)]
+                                if r2 <= r2_threshold:
+                                    unmatch[m].add(n)
+                                    unmatch[n].add(m)
+                            else:
+                                raise ValueError("pair not found")
+                        
+                    cliques = self._find_cliques(unmatch)
+                    for clique in cliques:
+                        if len(clique) >= min_site_number - 1:
+                            clique = list(clique)
+                            clique.sort()
+                            headless_mh_formal = [headless_mh_waiting_check[i] for i in clique]
+                            outfile.write(f'{front_site_info[1]}|{front_site_info[0]}-{"-".join(headless_mh_formal)}\n')
         infile1.close()
         infile2.close()
         outfile.close()
         
-        print(f"Microhaplotype generation completed, output file: {output}")
+        print(f'Microhaplotype generation completed, output file: {output}')
         return output 
